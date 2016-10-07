57cb524672ccfaa3b599337cfeae25786b21c727
mkierc <urban.pl@gmail.com>
2016-10-07 09:42:06 +0200

    variable and method names refactor, added some comments

--- plotter.py
+++ plotter.py
@@ -10,6 +10,7 @@ from pprint import pprint
 # todo: automatyczne kopiowanie wykresu do public_html
 # todo: dodawanie nowego wykresu jako head-file
 
+# read data from datafile
 if use_old_data:
     with open('old-datafile.raw', 'r') as input_handle:
         input_dict = pickle.loads(input_handle.read())
@@ -19,27 +20,29 @@ else:
 
 pprint(input_dict)
 
-output_dict = dict()
+# remap data from datetime:{product:price} to product:{datetime:price} format
+remapped_dict = dict()
 for date_key in input_dict.items():
-    for lol in date_key[1].items():
-        if lol[0] not in output_dict:
-            output_dict[lol[0]] = dict()
-        output_dict.get(lol[0]).update({datetime.datetime.strptime(date_key[0], '%Y.%m.%d %H:%M:%S'): lol[1]})
+    for product_entry in date_key[1].items():
+        if product_entry[0] not in remapped_dict:
+            remapped_dict[product_entry[0]] = dict()
+        remapped_dict.get(product_entry[0]).update({datetime.datetime.strptime(date_key[0], '%Y.%m.%d %H:%M:%S'): product_entry[1]})
 
-pprint(output_dict)
+pprint(remapped_dict)
 
+# configure the plot
 width = 1500
 height = 600
 ppi = 72
 plt.figure(figsize=(width / ppi, height / ppi), dpi=ppi)
 
-for product in output_dict:
-    # product == 'GTX 1080'...
-    raw_data = output_dict.get(product)
+# serve data to plotter
+for product_name in remapped_dict:
+    raw_data = remapped_dict.get(product_name)
     sorted_data = sorted(raw_data.items())
     x, y = zip(*sorted_data)
     plt.plot(x, y)
 
-
+# draw plot to file
 filename = 'plots/' + datetime.datetime.now().strftime('%Y-%m-%d') + '.png'
 plt.savefig(filename, dpi=ppi, bbox_inches='tight')
--- scraper.py
+++ scraper.py
@@ -33,23 +33,23 @@ class Scraper:
 
         print 'previous datafile: ' + str(input_dict)
 
-        # dodajemy nowe ceny do slownika
+        # get new prices
         current_prices = {}
         for item in product_dict:
             key = item
-            value = self.get_cena(product_dict.get(item))
+            value = self.get_price(product_dict.get(item))
             current_prices.update({key: value})
 
         print 'new data: ' + str(current_prices)
 
-        # dopisujemy nowe ceny do mapy
+        # add new prices to the dictionary
         input_dict.update({current_date: current_prices})
 
         print 'new datafile: ' + str(input_dict)
         if not dont_save_data:
             self.write_datafile(input_dict)
 
-    # element do podp*dolenia: cena
+    # element to "scrape" the price off of ;)
     #  <span style="color:inherit;font-size:inherit;"
     #   property="gr:hasCurrencyValue"
     #   datatype="xsd:float"
@@ -58,9 +58,9 @@ class Scraper:
     #       1859,00
     # </span>
     @staticmethod
-    def get_cena(adres_strony):
+    def get_price(url):
         if not dont_download_data:
-            # timeoutem zeby morele sie nie czepialy o scraping
+            # timeout to avoid cloudflare from banning the scrapers ip
             time.sleep(5)
             proxies = {}
             if with_proxy:
@@ -68,21 +68,21 @@ class Scraper:
                     'http': 'http://192.168.132.10:8080',
                     'https': 'http://192.168.132.10:8080',
                 }
-            page = requests.get(adres_strony, proxies=proxies)
+            page = requests.get(url, proxies=proxies)
             tree = html.fromstring(page.content)
-            cena = tree.xpath('//span[@itemprop="price"]/text()')
-            return cena[0]
+            price = tree.xpath('//span[@itemprop="price"]/text()')
+            return price[0]
         else:
             return round(random.uniform(2000, 6000), 2)
 
-    # odczytujemy dane z datafile.raw
+    # read data from datafile.raw
     @staticmethod
     def get_current_datafile():
         with open(name='datafile.raw', mode='r') as input_handle:
             input_dict = pickle.loads(input_handle.read())
         return input_dict
 
-    # zapisujemy dane do datafile.raw
+    # write data to datafile.raw
     @staticmethod
     def write_datafile(new_dict):
         with open(name='datafile.raw', mode='w') as output_handle:
