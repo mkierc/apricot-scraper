e432e6b672e78000b73f9553f14d3b28c9e2f947
mkierc <urban.pl@gmail.com>
2016-11-17 13:39:01 +0100

    fixed the scraping method, after interface changes on scraped website

--- products.py
+++ products.py
@@ -1,12 +1,12 @@
 product_dict = {
-    # 'GTX 1080': 'https://www.morele.net/karta-graficzna-gigabyte-geforce-cuda-gtx1080-xtreme-8gb-gddr5-256-bit-dvi-hdmi-3xdp-gv-n1080xtreme-w-8gd-987876/',
-    # 'GTX 980TI': 'https://www.morele.net/karta-graficzna-msi-geforce-gtx-980ti-6gb-gddr5-384-bit-3x-dp-hdmi-dvi-i-gtx980ti-6gd5-v1-754511/'
-    '1. Gigabyte GTX 1080 Xtreme': '',
-    '2. EVGA GTX 980 Ti Hybrid': '',
-    '3. EVGA GTX 980 Hybrid': '',
-    '4. EVGA GTX 970 Hybrid': '',
-    '5. Palit GTX 1080 ECO': '',
-    '6. MSI GTX 980 Ti ECO': '',
-    '7. Gainward GeForce GTX 980': '',
-    '8. Gigabyte GTX 970 ECO': ''
+    'GTX 1080': 'https://www.morele.net/karta-graficzna-gigabyte-geforce-cuda-gtx1080-xtreme-8gb-gddr5-256-bit-dvi-hdmi-3xdp-gv-n1080xtreme-w-8gd-987876/',
+    'GTX 980TI': 'https://www.morele.net/karta-graficzna-msi-geforce-gtx-980ti-6gb-gddr5-384-bit-3x-dp-hdmi-dvi-i-gtx980ti-6gd5-v1-754511/'
+    # '1. Gigabyte GTX 1080 Xtreme': '',
+    # '2. EVGA GTX 980 Ti Hybrid': '',
+    # '3. EVGA GTX 980 Hybrid': '',
+    # '4. EVGA GTX 970 Hybrid': '',
+    # '5. Palit GTX 1080 ECO': '',
+    # '6. MSI GTX 980 Ti ECO': '',
+    # '7. Gainward GeForce GTX 980': '',
+    # '8. Gigabyte GTX 970 ECO': ''
 }
